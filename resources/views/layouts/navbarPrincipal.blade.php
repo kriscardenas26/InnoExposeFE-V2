08aab4affd67ddf16886d61d3256a50f88dbd563
kriscardenas26 <kriscardenas260103@gmail.com>
2023-10-13 13:30:08 -0600

    Arreglando errores de la extensiòn

--- resources/views/layouts/navbarPrincipal.blade.php
+++ resources/views/layouts/navbarPrincipal.blade.php
@@ -21,6 +21,19 @@
                     </div>
                     <a href="about" class="nav-item nav-link">Sobre Nosotros</a>
                     <a href="class" class="nav-item nav-link">Tutoriales</a>
+                    <!-- @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif -->
                     <!-- <a href="team" class="nav-item nav-link">Teachers</a> -->
                     <!-- <a href="gallery" class="nav-item nav-link">Gallery</a> -->
                 </div>
